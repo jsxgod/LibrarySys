321948e2e1ec3cab3e3c623aed5c9144b366a4f9
jsxgod <smykoo7@gmail.com>
2018-01-06 16:17:13 +0100

    first GUI commit

---
+++ Client/src/Controllers/LoginController.java
@@ -0,0 +1,20 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+
+    public LoginController() throws IOException{
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
---
+++ Client/src/LibrarySystem.java
@@ -0,0 +1,32 @@
+import Controllers.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LibrarySystem extends Application {
+
+    private Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        window = primaryStage;
+
+        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("scenes/login.fxml"));
+        Parent loginRoot = loginLoader.load();
+
+        Scene loginScene = new Scene(loginRoot,600,300);
+
+        LoginController loginController = loginLoader.getController();
+
+        primaryStage.setScene(loginScene);
+        primaryStage.show();
+    }
+}
