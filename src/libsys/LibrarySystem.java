7a604bf48de9666860b19abcd6524ee856788066
jsxgod <smykoo7@gmail.com>
2018-01-06 16:38:44 +0100

    second GUI commit

--- src/libsys/LibrarySystem.java
+++ src/libsys/LibrarySystem.java
@@ -1,3 +1,5 @@
+package libsys;
+
 import Controllers.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +21,7 @@ public class LibrarySystem extends Application {
     public void start(Stage primaryStage) throws IOException {
         window = primaryStage;
 
-        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("scenes/login.fxml"));
+        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("/scenes/login.fxml"));
         Parent loginRoot = loginLoader.load();
 
         Scene loginScene = new Scene(loginRoot,600,300);
